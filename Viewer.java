d329c509db191324a90b36ef038a7bd3ddeb6c8f
lukedoukakis <35055929+lukedoukakis@users.noreply.github.com>
2018-02-01 16:31:15 -0800

    Add files via upload

---
+++ Viewer.java
@@ -0,0 +1,23 @@
+
+public class Viewer {
+	
+	private String name;
+	
+	
+	
+	//------getters and setters
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name=name;
+	}
+	
+	//--------------------------
+	
+	public int rateMovie(Movie movie, int rating){  //this will work with your movie class
+		return rating;
+	}
+}
