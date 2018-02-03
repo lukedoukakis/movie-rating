0989ad4164fdfd3a9fed24593c8bd74bf5f1da00
lukedoukakis <35055929+lukedoukakis@users.noreply.github.com>
2018-02-02 20:26:16 -0800

    Update Driver.java
    sets stuff up and goes to the point where the user picks a movie and rates it, but doesn't put that in the 2d array yet

--- Driver.java
+++ Driver.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
-import Random;
+import java.util.Random;
 
 public class Driver {
 
-	Scanner s = new Scanner(System.in);
-	Random r = new Random();
-
-	public int generateRandom(){
-		int n = r.nextInt(5);
-	}
+	static Scanner s = new Scanner(System.in);
+	static Random r = new Random();
 
 	
 	public static void main(String[] args) {
+		
+		//initializes movies and 2d array
+		
 		Movie kingKong=new Movie("King Kong", 'a');
 		Movie theNotebook=new Movie("The Notebook", 'd');
 		Movie sideways=new Movie("Sideways", 'd');
@@ -19,14 +18,64 @@ public class Driver {
 		Movie inception=new Movie("Inception", 't');
 			
 		String[][] theatre = new String[5][10];
-		Movie[] movies = {kingKong, theNotebook, sideways, theMask, inception}
+		Movie[] movies = {kingKong, theNotebook, sideways, theMask, inception};
 		
+		//-------------------------------------------
 		
-		for(int i=0;i<theatre.length;i++){
-			theatre[i]=movies[i];
+		////sets up left side of theatre as movie names
+		
+		for(int i=0;i<theatre.length;i++){ 
+			theatre[i][0]=movies[i].toString();
 		}
 		
-		System.out.println
+		/*for(int i=0;i<theatre.length;i++){
+			for(int j =18; j>=theatre[i][0].length();j--){
+				theatre[i][0] = theatre[i][0]+ " ";
+			}
+		}*/ //probably not needed, meant to set all the cells on the lefthand side to the same length
+		
+		//---------------------------------------------------------------------------
+		
+		Viewer v = new Viewer();
+		
+		int movieChoice;
+		int ratingChoice;
+		
+		System.out.println("Welcome reviewer, what is your name? ");
+		v.setName(s.nextLine());
+		
+		do{
+			System.out.println(v.getName()+", Type the number for the movie you'd like to rate: ");
+			System.out.println("\t1) " + theatre[0][0]);
+			System.out.println("\t2) " + theatre[1][0]);
+			System.out.println("\t3) " + theatre[2][0]);
+			System.out.println("\t4) " + theatre[3][0]);
+			System.out.println("\t5) " + theatre[4][0]);
+			
+			movieChoice = s.nextInt();
+			
+		} while(movieChoice > 6 || movieChoice < 0);
+		
+		do{
+			System.out.println("What would you like to rate " + theatre[movieChoice-1][0] + "? Enter a number from 0-100: ");
+			
+			ratingChoice = s.nextInt();
+		
+		} while(ratingChoice > 100 || ratingChoice < 0);
+		
+		System.out.println("You rated "+theatre[movieChoice][0] + " " + ratingChoice + "/100.");
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
 		
 			
 		
